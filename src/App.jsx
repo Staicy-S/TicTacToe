925e29c53eb33c6d326cd67fdb8078f1c8cfdf3b
Ralf Siewert <ralf@actyvy.st>
2024-01-31 14:21:48 +0100

    display board

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { Board } from "./components/Board";
+
+const dummyBoard = [
+  [-1, -1, -1],
+  [-1, -1, -1],
+  [-1, -1, 1],
+];
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Board board={dummyBoard} />
+    </>
+  );
 }
 
 export default App;
